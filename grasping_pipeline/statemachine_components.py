bce9219bc0eb1585b9a07e5f4f2b6e8223c9056d
StefanDeimel <stefan.deimel@aon.at>
2026-08-05 11:08:10 +0200

    update bringup script

--- grasping_pipeline/statemachine_components.py
+++ grasping_pipeline/statemachine_components.py
@@ -44,14 +44,14 @@ def get_robot_setup_sm(setup_waypoint, node):
     '''
     
     sm = yasmin.StateMachine(outcomes=['setup_succeeded', 'setup_failed'], handle_sigint=True)
-    #sm.add_state(
-    #    "GO_TO_TABLE",
-    #    setup_waypoint, 
-    #    transitions={
-    #        "aborted": "GO_TO_TABLE",
-    #        "succeeded" : "GO_TO_NEUTRAL",
-    #    },
-    #)
+    sm.add_state(
+       "GO_TO_TABLE",
+       setup_waypoint, 
+       transitions={
+           "aborted": "GO_TO_TABLE",
+           "succeeded" : "GO_TO_NEUTRAL",
+       },
+    )
     sm.add_state(
         "GO_TO_NEUTRAL",
         GoToNeutral(node), 
--- tmux_scripts/pipeline_bringup.sh
+++ tmux_scripts/pipeline_bringup.sh
@@ -16,42 +16,30 @@ tmux select-window -t $SESSION:0
 tmux split-window -h
 tmux split-window -h
 
-
 tmux select-pane -t 0
 tmux send-keys "source /root/ros2_ws/install/setup.bash" C-m
 tmux send-keys "source /root/hsr_ros2_ws/install/setup.bash" C-m
 tmux send-keys "ros2 launch grasping_pipeline grasping_pipeline_statemachine.launch.py"
 
 tmux select-pane -t 1
-tmux send-keys "hsrb_mode" C-m
+tmux send-keys "source /root/ros2_ws/install/setup.bash" C-m
 tmux send-keys "source /root/hsr_ros2_ws/install/setup.bash" C-m
 tmux send-keys "ros2 launch grasping_pipeline grasping_pipeline_server.launch.py"
-# tmux split-window -v
-# tmux select-pane -t 2
-# tmux send-keys "hsrb_mode" C-m
-# tmux send-keys "source $SCRIPT_DIR/../../../devel/setup.bash" C-m
-# tmux send-keys "roslaunch grasping_pipeline grasping_pipeline_params.launch"
 
-tmux select-pane -t 3
-tmux send-keys "hsrb_mode" C-m
-tmux send-keys rviz2 -d $(ros2 pkg prefix grasping_pipeline)/share/grasping_pipeline/config/grasping_pipeline.rviz
-tmux split-window -v
-tmux select-pane -t 4
-tmux send-keys "ssh v4r@hsrb.local" C-m
-tmux send-keys "startup && table"
+tmux select-pane -t 2
+tmux send-keys "source /root/ros2_ws/install/setup.bash" C-m
+tmux send-keys "source /root/hsr_ros2_ws/install/setup.bash" C-m
+tmux send-keys "ros2 run grasping_pipeline userinput_publisher"
 
 tmux select-window -t $SESSION:1
-
-tmux select-pane -t 0
 tmux send-keys "ssh v4r@hsrb.local" C-m
-tmux send-keys "source ~/demos/devel/setup.bash"
-tmux send-keys enter
+tmux send-keys "docker exec -it docker.humble.robot.service /ros_entrypoint.sh /bin/bash" C-m
+tmux send-keys "source /root/ros2_ws/install/setup.bash" C-m
 tmux send-keys "ros2 launch hsrb_moveit_config hsrb_demo.launch.py"
 
+tmux rename-window 'grasping'
 tmux select-window -t $SESSION:0
 tmux select-pane -t 2
-
-tmux rename-window 'grasping'
 
 # Attach to session
 tmux -2 attach-session -t $SESSION
